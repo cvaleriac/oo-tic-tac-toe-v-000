f915d14c925bdf9653f1a31821f988afd78c756e
valeria campilongo <cvaleriac@hotmail.it>
2018-03-25 00:12:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ def position_taken?(index)
   else #(board[index] == "X" || board[index]== "O")
     return true
   end
-  def valid_move?
+  def valid_move?(input_to_index)
 if index.between?(0,8) && !position_taken?(index)
   return true
 else
